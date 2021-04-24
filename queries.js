87d2d4de93d6aa21f3d0eb951f3b96e13a874bf3
Jacob T <jacob.tingling@protonmail.com>
2021-04-24 11:25:15 -0400

    added search by game Id query string.

--- queries.js
+++ queries.js
@@ -1,7 +1,7 @@
 const query = {
     "cover": `game,height,width,image_id,url;`,
     "company": `name,logo,description,published,websites,updated_at;`,
-    "game": `name,platforms,category,cover,rating,rating_count,release_dates,summary,screenshots.url,videos;`
+    "game": `name,platforms,category,screenshots.*,rating,rating_count,release_dates,summary,screenshots.url,videos;`
 }
 
 const uris = {
@@ -12,8 +12,14 @@ const uris = {
 
 const searchGames = (gameName) => {
     let search = "search " + `"${gameName}"` + "; " + "fields " + query.game;
-    
     return search;
 }
+
+const searchByGameId = (gameId) => {
+    let id = `where id= ${gameId}; fields ${query.game}`;
+    return id;
+}
+
 exports.getUris = uris;
 exports.createQuery = searchGames;
+exports.searchById = searchByGameId
