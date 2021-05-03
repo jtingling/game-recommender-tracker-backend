9411d8d30269d4e7ed9c14c089296ed357e711ed
Jacob T <jacob.tingling@protonmail.com>
2021-05-03 12:07:06 -0400

    Modified content header to app/json.

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.get('/boxart/:coverId', (req, res) =>{
 
 app.get('/video/:gameName', (req, res) =>{
     getGameVideo(req.params.gameName, youtubeAPI.getTrailers)
-    .then((data)=> {console.log(data); res.status(200).json(data)})
+    .then((data)=> res.status(200).json(data))
     .catch(e=> res.status(403).send(e.message + " Daily Quota Exceeded"))
 })
 
